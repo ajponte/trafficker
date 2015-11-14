0852c6dfc6e4d9d99a26c5cd482e07f5cee7935b
Alan Ponte <aponte@Alans-MacBook-Pro.local>
2015-11-13 22:45:51 -0800

    first commit

---
+++ trafficker/src/main/java/com/upalate/trafficker/core/TraffickerCoreApplication.java
@@ -0,0 +1,12 @@
+package com.upalate.trafficker.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraffickerCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraffickerCoreApplication.class, args);
+    }
+}
